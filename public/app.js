b1020104fe56d912932f1e7b739b18255a8cee1b
coloradokim <schlesinger.kim@gmail.com>
2017-03-11 16:48:19 -0700

    user can search by year degree was awarded

--- public/app.js
+++ public/app.js
@@ -1,14 +1,16 @@
-$( document ).ready(function() {
-  console.log('hi!');
-  });
-
-  $.ajax({
-      url: "https://data.colorado.gov/resource/yt5k-hawq.json?year=2015",
-      type: "GET",
-      data: {
-        "$limit" : 5000,
-        "$$app_token" : "Y0bDVvQ1EATIlQ2bHbPv30kDR"
-      }
-  }).done(function(data) {
-    console.log(data);
-  });
+$("form").submit(function(event) {
+    const $year = $("#yearAwarded").val()
+    $(".showMe").append("<p>" + $year + "</p>")
+    event.preventDefault();
+    
+    $.ajax({
+        url: "https://data.colorado.gov/resource/yt5k-hawq.json?year=" + $year,
+        type: "GET",
+        data: {
+            "$limit": 5000,
+            "$$app_token": "Y0bDVvQ1EATIlQ2bHbPv30kDR"
+        }
+    }).done(function(data) {
+        console.log(data);
+    });
+});
